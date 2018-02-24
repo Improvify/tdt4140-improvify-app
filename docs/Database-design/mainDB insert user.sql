df8c17e40d7c0812037b7e639b5de58c0f13314b
dennisjl <dennisjl@stud.ntnu.no>
2018-02-24 15:02:56 +0100

    Create a user entity in the database. Fixes #5

---
+++ docs/Database-design/mainDB insert user.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM ecosystem.useraccount
+LIMIT 0, 1000
+
+-- Date: 2018-02-24 14:58
+*/
+INSERT INTO `useraccount` (`id`, `firstname`, `lastname`, `height`, `birthdate`, `username`, `password`, `email`) VALUES
+  (1, 'claus', 'martinsen', 188.00, '1996-06-25', 'clauswrm', '123456678', 'clauswrm@gmail.com');
