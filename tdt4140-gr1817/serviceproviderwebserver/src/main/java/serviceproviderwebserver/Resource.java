caf8333201050e599fa4e6df4d4f67131dea6c00
Fredrik Jenssen <fredjen.contactor@gmail.com>
2018-02-24 13:34:51 +0100

    REST framework using Jetty and Jersey

---
+++ tdt4140-gr1817/serviceproviderwebserver/src/main/java/serviceproviderwebserver/Resource.java
@@ -0,0 +1,16 @@
+package serviceproviderwebserver;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("api")
+public class Resource {
+    @GET
+    @Path("hello")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String helloWorld() {
+        return "Hello, world!";
+    }
+}
