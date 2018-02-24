7fc05b900a2adbc298e06094f598875ec28dae96
Fredrik Jenssen <fredjen.contactor@gmail.com>
2018-02-24 13:34:51 +0100

    REST framework using Jetty and Jersey

---
+++ tdt4140-gr1817/serviceproviderwebserver/src/main/java/serviceproviderwebserver/App.java
@@ -0,0 +1,30 @@
+package serviceproviderwebserver;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletContainer;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        ResourceConfig config = new ResourceConfig();
+        config.packages("serviceproviderwebserver");
+        ServletHolder servlet = new ServletHolder(new ServletContainer(config));
+
+        Server server = new Server(2222);
+        ServletContextHandler context = new ServletContextHandler(server, "/*");
+        context.addServlet(servlet, "/*");
+
+        try {
+            server.start();
+            server.join();
+        } catch (Exception e){
+            server.stop();
+            server.destroy();
+        }
+
+    }
+
+}
