aa570986bbe6f2e524a21063e20681c947766256
Kristian Rekstad <krirek@stud.ntnu.no>
2018-03-06 13:05:18 +0100

    Add database test

--- tdt4140-gr1817/ecosystem.persistence/src/test/java/tdt4140/gr1817/ecosystem/persistence/repositories/mysql/MySqlUserRepositoryTest.java
+++ tdt4140-gr1817/ecosystem.persistence/src/test/java/tdt4140/gr1817/ecosystem/persistence/repositories/mysql/MySqlUserRepositoryTest.java
@@ -1,55 +1,95 @@
 package tdt4140.gr1817.ecosystem.persistence.repositories.mysql;
 
 import lombok.extern.slf4j.Slf4j;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.hsqldb.jdbcDriver;
 import org.hsqldb.server.Server;
 import org.junit.*;
+import org.mockito.Mockito;
 import org.slf4j.LoggerFactory;
 import tdt4140.gr1817.ecosystem.persistence.data.User;
 import tdt4140.gr1817.ecosystem.persistence.repositories.mysql.util.HsqldbRule;
 
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.sql.*;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
+@Slf4j
 public class MySqlUserRepositoryTest {
 
     @Rule
-    HsqldbRule hsqldb = new HsqldbRule();
+    public HsqldbRule hsqldb = new HsqldbRule();
+
+    @Test
+    public void hsqldbWorks() throws Exception {
+        try (
+                final Connection connection = hsqldb.getConnection();
+                Statement statement = connection.createStatement()
+        ) {
+            final int changed = statement.executeUpdate("INSERT INTO useraccount VALUES "
+                    + "(1, 'kek', 'Lord', 2.0, DATE '1997-01-01', 'keks', '123', 'hehe@gog.com')");
+
+            assertThat(changed, is(1));
+
+            try (ResultSet resultSet = statement.executeQuery("SELECT * FROM useraccount")) {
+                while (resultSet.next()) {
+                    assertThat(resultSet.getString("username"), is("keks"));
+                }
+            }
+
+        }
+    }
 
     @Test
     public void shouldAddUser() throws Exception {
-        // BAD TEST. Just written as an example. Maybe use integration tests instead.
-
         // Given
-        Connection connection = mock(Connection.class);
-        PreparedStatement preparedStatement = mock(PreparedStatement.class);
-        when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
-        MySqlUserRepository repository = new MySqlUserRepository(() -> connection);
-        User user = createUser(1);
+        final Connection connectionSpy = Mockito.spy(hsqldb.getConnection()); // Will be closed
+        MySqlUserRepository repository = new MySqlUserRepository(() -> connectionSpy);
+        final Date birthDate = new GregorianCalendar(1995, 8, 20).getTime();
+
+        User user = createUser()
+                .id(1)
+                .birthDate(birthDate)
+                .build();
 
         // When
         repository.add(user);
 
         // Then
-        verify(connection).prepareStatement(anyString());
-        verify(preparedStatement, never()).setObject(eq(0), any());
-        verify(preparedStatement).setObject(anyInt(), eq("Test"));
-        verify(preparedStatement).setObject(anyInt(), eq("Person"));
-        verify(preparedStatement).setObject(anyInt(), eq(1));
+        verify(connectionSpy).prepareStatement(anyString());
+        verify(connectionSpy).close();
+
+        try (Connection connection = hsqldb.getConnection()) {
+            try (ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM useraccount WHERE  id = 1")) {
+                while (resultSet.next()) {
+                    assertThat(resultSet.getString("firstname"), is("Test"));
+                    assertThat(resultSet.getString("lastname"), is("Person"));
+                    assertThat(resultSet.getString("email"), is("test@test.com"));
+                    assertThat(resultSet.getString("password"), is("123"));
+                    final long birthDate1 = resultSet.getDate("birthDate").getTime();
+                    assertThat(birthDate1, is(birthDate.getTime()));
+                }
+            }
+        }
     }
 
-    private static User createUser(int id) {
+    private static User.UserBuilder createUser() {
         return User.builder()
-                .id(id)
+                .id(1)
                 .firstName("Test")
                 .lastName("Person")
                 .birthDate(new Date())
-                .build();
+                .username("testuser")
+                .password("123")
+                .email("test@test.com")
+                .height(175.5f);
     }
 }
