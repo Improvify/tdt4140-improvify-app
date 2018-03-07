930f2635a2129043660dba25e42acb257b7ea917
Kristian Rekstad <krirek@stud.ntnu.no>
2018-03-07 21:32:44 +0100

    Refactor App. Reduce logging from jetty

--- tdt4140-gr1817/serviceprovider.webserver/src/main/java/serviceproviderwebserver/App.java
+++ tdt4140-gr1817/serviceprovider.webserver/src/main/java/serviceproviderwebserver/App.java
@@ -1,12 +1,13 @@
 package serviceproviderwebserver;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 import com.google.gson.Gson;
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import com.google.inject.Provides;
-import com.google.inject.servlet.GuiceFilter;
-import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
@@ -16,80 +17,110 @@ import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
 import org.jvnet.hk2.guice.bridge.api.GuiceBridge;
 import org.jvnet.hk2.guice.bridge.api.GuiceIntoHK2Bridge;
+import org.slf4j.LoggerFactory;
+import tdt4140.gr1817.ecosystem.persistence.repositories.RestingHeartRateRepository;
 import tdt4140.gr1817.ecosystem.persistence.repositories.UserRepository;
+import tdt4140.gr1817.ecosystem.persistence.repositories.WeightRepository;
+import tdt4140.gr1817.ecosystem.persistence.repositories.WorkoutSessionRepository;
+import tdt4140.gr1817.ecosystem.persistence.repositories.mysql.MySqlRestingHeartRateRepository;
 import tdt4140.gr1817.ecosystem.persistence.repositories.mysql.MySqlUserRepository;
+import tdt4140.gr1817.ecosystem.persistence.repositories.mysql.MySqlWeightRepository;
+import tdt4140.gr1817.ecosystem.persistence.repositories.mysql.MySqlWorkoutSessionRepository;
+import tdt4140.gr1817.ecosystem.persistence.repositories.mysql.guice.MySqlConnectionModule;
 
-import javax.servlet.DispatcherType;
 import javax.ws.rs.core.Feature;
-import javax.ws.rs.core.FeatureContext;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.EnumSet;
 
+@Slf4j
 public class App {
 
-    private App() {
+
+    public static void main(String[] args) {
+        // Jetty spams too much. Increase level for less spam
+        ((Logger) LoggerFactory.getLogger("org.eclipse.jetty")).setLevel(Level.INFO);
+
+
+        final Injector injector = createInjector();
+        final App app = new App(2222, injector);
+
+        app.start();
     }
 
-    public static void main(String[] args) throws Exception {
-        Injector injector = Guice.createInjector(new AbstractModule() {
-            @Override
-            protected void configure() {
-                bind(UserRepository.class).to(MySqlUserRepository.class);
-                //bind(WorkoutSessionRepository.class).toInstance(null);
-                bind(Gson.class).toInstance(new Gson());
-            }
+    private static Injector createInjector() {
+        //TODO get database configuration from system env or args
 
-            @Provides
-            public Connection provideConnection() throws SQLException {
-                MysqlDataSource source = new MysqlDataSource();
-                source.setDatabaseName("ecosystem");
-                return source.getConnection("root", "root");
-            }
-        });
-/*
-        GuiceServletContextListener guiceServletContextListener = new GuiceServletContextListener() {
-            @Override
-            protected Injector getInjector() {
-                return injector;
-            }
-        };
-*/
+        Injector injector = Guice.createInjector(
+                new MySqlConnectionModule("root", "root", "localhost",
+                        "ecosystem", 3306),
+//                new MySqlRepositoryModule(),
+                new AbstractModule() {
+                    @Override
+                    protected void configure() {
+                        // FIXME: replace this module with MySqlRepistoryModule when it's done
+                        bind(RestingHeartRateRepository.class).to(MySqlRestingHeartRateRepository.class);
+                        bind(UserRepository.class).to(MySqlUserRepository.class);
+                        bind(WeightRepository.class).to(MySqlWeightRepository.class);
+                        bind(WorkoutSessionRepository.class).to(MySqlWorkoutSessionRepository.class);
+                    }
+                },
+                new AbstractModule() {
+                    @Override
+                    protected void configure() {
+                        bind(Gson.class).toInstance(new Gson());
+                    }
+                });
 
+        return injector;
+    }
 
-        ResourceConfig config = new ResourceConfig();
-        config.packages("serviceproviderwebserver");
-        config.register(new Feature() {
-            @Override
-            public boolean configure(FeatureContext context) {
-                ServiceLocator serviceLocator = ServiceLocatorProvider.getServiceLocator(context);
-                GuiceBridge.getGuiceBridge().initializeGuiceBridge(serviceLocator);
-                GuiceIntoHK2Bridge guiceBridge = serviceLocator.getService(GuiceIntoHK2Bridge.class);
-                guiceBridge.bridgeGuiceInjector(injector);
-                return true;
-            }
-        });
-        ServletContainer jerseyServlet = new ServletContainer(config);
+    private final Server server;
+    private final Injector injector;
 
-        ServletHolder servlet = new ServletHolder(jerseyServlet);
+    private App(int port, @NonNull Injector injector) {
+        server = new Server(port);
+        this.injector = injector;
+    }
 
-        Server server = new Server(2222);
-        ServletContextHandler context = new ServletContextHandler(server, "/*");
-
-        context.addFilter(GuiceFilter.class, "/*", EnumSet.of(DispatcherType.REQUEST));
-        context.addServlet(servlet, "/*");
-
-
-        //context.addEventListener(guiceServletContextListener);
-
+    /**
+     * Starts the web server.
+     * <p>
+     * Blocking method, so this will not return untill the server shuts down
+     */
+    public void start() {
+        initialize();
         try {
             server.start();
             server.join();
         } catch (Exception e) {
-            server.stop();
+            try {
+                server.stop();
+            } catch (Exception ex) {
+                log.error("Failed to stop server", ex);
+            }
             server.destroy();
+            log.error("Server crashed!", e);
         }
-
     }
 
+    private void initialize() {
+        // Enable Jersey and our resources
+
+        ResourceConfig config = new ResourceConfig();
+        config.packages("serviceproviderwebserver");
+        config.register((Feature) context -> {
+            // Jersey uses HK2 for dependency injection in resources. We replace that with Guice here,
+            // since we don't want more frameworks for doing the same, and other modules use Guice.
+            ServiceLocator serviceLocator = ServiceLocatorProvider.getServiceLocator(context);
+            GuiceBridge.getGuiceBridge().initializeGuiceBridge(serviceLocator);
+            GuiceIntoHK2Bridge guiceBridge = serviceLocator.getService(GuiceIntoHK2Bridge.class);
+            guiceBridge.bridgeGuiceInjector(injector);
+            return true;
+        });
+
+        ServletContainer jerseyServlet = new ServletContainer(config);
+        ServletHolder servlet = new ServletHolder(jerseyServlet);
+
+        ServletContextHandler context = new ServletContextHandler(server, "/*");
+        context.addServlet(servlet, "/*");
+//        context.addFilter(GuiceFilter.class, "/*", EnumSet.of(DispatcherType.REQUEST));
+    }
 }
