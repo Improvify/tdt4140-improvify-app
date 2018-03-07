c362608a439974ac62ad40834074d596dcedfae8
Fredrik Jenssen <fredjen.contactor@gmail.com>
2018-03-07 11:01:39 +0100

    Adds not-completed GoalResource and WorkoutSessionResource tests

--- tdt4140-gr1817/serviceproviderwebserver/src/test/java/GoalResourceTest.java
+++ tdt4140-gr1817/serviceproviderwebserver/src/test/java/GoalResourceTest.java
@@ -3,11 +3,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 import serviceproviderwebserver.GoalResource;
-import serviceproviderwebserver.UserResource;
 import tdt4140.gr1817.ecosystem.persistence.data.Goal;
 import tdt4140.gr1817.ecosystem.persistence.data.User;
 import tdt4140.gr1817.ecosystem.persistence.repositories.GoalRepository;
-import tdt4140.gr1817.ecosystem.persistence.repositories.UserRepository;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -36,7 +34,24 @@ public class GoalResourceTest {
         goalResource.createGoal(json);
 
         // Then
-        verify(rep).add(Mockito.eq(goal));;
+        verify(rep).add(Mockito.eq(goal));
+        verifyNoMoreInteractions(rep);
+    }
+
+    @Test
+    public void shouldFailToAddGoal(){
+        // Given
+        Gson gson = new Gson();
+        GoalResource goalResource = new GoalResource(rep, gson);
+        Goal goal = createGoal();
+        String s = goal.toString();
+
+        // When
+        goalResource.createGoal(s);
+
+        // Then
+
+        verify(rep).add(Mockito.eq(goal));
         verifyNoMoreInteractions(rep);
     }
 
---
+++ tdt4140-gr1817/serviceproviderwebserver/src/test/java/WorkoutSessionResourceTest.java
@@ -0,0 +1,67 @@
+import com.google.gson.Gson;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import serviceproviderwebserver.GoalResource;
+import serviceproviderwebserver.WorkoutSessionResource;
+import tdt4140.gr1817.ecosystem.persistence.data.Goal;
+import tdt4140.gr1817.ecosystem.persistence.data.User;
+import tdt4140.gr1817.ecosystem.persistence.data.WorkoutSession;
+import tdt4140.gr1817.ecosystem.persistence.repositories.GoalRepository;
+import tdt4140.gr1817.ecosystem.persistence.repositories.WorkoutSessionRepository;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+public class WorkoutSessionResourceTest {
+    WorkoutSessionRepository rep;
+
+    @Before
+    public void setUp() throws Exception {
+        rep = Mockito.mock(WorkoutSessionRepository.class);
+    }
+
+    @Test
+    public void shouldAddWorkoutSession() {
+        // Given
+        Gson gson = new Gson();
+        WorkoutSessionResource workoutSessionResource = new WorkoutSessionResource(rep, gson);
+        WorkoutSession workoutSession = createWorkoutSession();
+        String json = gson.toJson(workoutSession);
+
+        // When
+        workoutSessionResource.createWorkoutSession(json);
+
+        // Then
+        verify(rep).add(Mockito.eq(workoutSession));
+        verifyNoMoreInteractions(rep);
+    }
+
+    @Test
+    public void shouldFailToAddWorkoutSession(){
+        // Given
+        Gson gson = new Gson();
+        WorkoutSessionResource workoutSessionResource = new WorkoutSessionResource(rep, gson);
+        WorkoutSession workoutSession = createWorkoutSession();
+        String s = workoutSession.toString();
+
+        // When
+        workoutSessionResource.createWorkoutSession(s);
+
+        // Then
+        verify(rep).add(Mockito.eq(workoutSession));
+        verifyNoMoreInteractions(rep);
+    }
+
+    private static WorkoutSession createWorkoutSession() {
+        Calendar calendar = new GregorianCalendar();
+        calendar.set(Calendar.MILLISECOND, 0); // JSON doesnt serialize milliseconds
+        Date date = calendar.getTime();
+        User user = new User(1,"hei", "bu", 2.5f, date, "hellu", "hshs", "123@hotmail.com");
+        return new WorkoutSession(1, date, 1, 12.5f, 140.4f, 170.3f, 12.3f, user);
+    }
+}
