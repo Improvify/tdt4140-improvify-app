3c7fc641b3c01efaf85b95d2d06d155a59db2305
Henfau <henrik.fauskanger@gmail.com>
2018-03-13 10:57:26 +0100

    add List of WorkoutPlanRows in WorkoutPlan

--- tdt4140-gr1817/ecosystem.persistence/src/main/java/tdt4140/gr1817/ecosystem/persistence/data/improvify/WorkoutPlan.java
+++ tdt4140-gr1817/ecosystem.persistence/src/main/java/tdt4140/gr1817/ecosystem/persistence/data/improvify/WorkoutPlan.java
@@ -3,6 +3,8 @@ package tdt4140.gr1817.ecosystem.persistence.data.improvify;
 import lombok.Data;
 import tdt4140.gr1817.ecosystem.persistence.data.User;
 
+import java.util.List;
+
 /**
  * A workout plan with a collection of exercises ("rows") to be executed during a session.
  * <p>
