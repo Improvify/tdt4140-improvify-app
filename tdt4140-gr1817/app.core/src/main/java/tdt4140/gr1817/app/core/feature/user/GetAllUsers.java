92dadb4e15a9b5843b9e88330921f6f557547dec
Kristian Rekstad <krirek@stud.ntnu.no>
2018-03-20 16:14:27 +0100

    Add use case to get all users

---
+++ tdt4140-gr1817/app.core/src/main/java/tdt4140/gr1817/app/core/feature/user/GetAllUsers.java
@@ -0,0 +1,33 @@
+package tdt4140.gr1817.app.core.feature.user;
+
+import lombok.NonNull;
+import tdt4140.gr1817.ecosystem.persistence.data.User;
+import tdt4140.gr1817.ecosystem.persistence.repositories.UserRepository;
+import tdt4140.gr1817.ecosystem.persistence.repositories.mysql.specification.GetAllUsersSpecification;
+
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Kristian Rekstad.
+ *
+ * @author Kristian Rekstad
+ */
+public class GetAllUsers {
+
+    private final UserRepository userRepository;
+
+    @Inject
+    public GetAllUsers(@NonNull UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAll() {
+        List<User> allUsers = userRepository.query(new GetAllUsersSpecification());
+        if (allUsers == null) {
+            allUsers = new ArrayList<>(0);
+        }
+        return allUsers;
+    }
+}
