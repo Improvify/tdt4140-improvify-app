1c2ddbf599d7e11becbe2173e74507d88ad9af7e
Henfau <henrik.fauskanger@gmail.com>
2018-04-13 15:01:18 +0200

    #43 start writing test case for VitalDataChartController. Fail. Should be ready to start integration

--- tdt4140-gr1817/app.ui/src/main/java/tdt4140/gr1817/app/ui/feature/userlist/VitalDataChartController.java
+++ tdt4140-gr1817/app.ui/src/main/java/tdt4140/gr1817/app/ui/feature/userlist/VitalDataChartController.java
@@ -40,35 +40,9 @@ public class VitalDataChartController {
     @FXML
     public void initialize() {
         lineChart.setTitle("hubba bubba");
-        testUpdate();
-    }
-
-    private void testUpdate() {
-
-
-        /*
-        Dette metoden eksisterer kun for å simulere henting fra Db
-
-        User user = new User(1,"abc","bcd",180,new Date(1980,10,10),
-                "bob","afskdj","asdf");
-        Weight w1 = new Weight(1,79,new Date(2018,11,10),user);
-        Weight w2 = new Weight(2,78,new Date(2018,12,10),user);
-        Weight w3 = new Weight(3,75,new Date(2019,1,10),user);
-        Weight w4 = new Weight(4,73,new Date(2019,2,10),user);
-        Weight w5 = new Weight(5,78,new Date(2019,3,10),user);
-        Weight w6 = new Weight(5,60,new Date(2019,8,25),user);
-
-        ArrayList<Weight> weights = new ArrayList<>();
-        weights.add(w1);
-        weights.add(w2);
-        weights.add(w3);
-        weights.add(w4);
-        weights.add(w5);
-        weights.add(w6);
-        updateLineChart(retrieveWeights(user),retrieveHeartRates(user));
-
-        */
-
+        User user = new User(1, "abc", "bcd", 180, new Date(1980, 10, 10),
+                "bob", "afskdj", "asdf");
+        updateLineChart(retrieveWeights(user), retrieveHeartRates(user));
     }
 
     private List retrieveWeights(User user) {
@@ -133,8 +107,8 @@ public class VitalDataChartController {
 
 
         yAxis.setAutoRanging(false);
-        yAxis.setLowerBound(yLowerBound);
-        yAxis.setUpperBound(yUpperBound);
+        yAxis.setLowerBound(yLowerBound - 10);
+        yAxis.setUpperBound(yUpperBound + 10);
 
         xAxis.setAutoRanging(false);
         xAxis.setLowerBound(xLowerBound);
