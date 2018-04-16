3c8f0b85f1416fb23e12c50c755432524bf15146
Ask Sommervoll <askas@stud.ntnu.no>
2018-04-16 22:07:57 +0200

    Fix test

--- tdt4140-gr1817/app.ui/src/it/java/tdt4140/gr1817/app/ui/feature/viewuser/ViewUserControllerIT.java
+++ tdt4140-gr1817/app.ui/src/it/java/tdt4140/gr1817/app/ui/feature/viewuser/ViewUserControllerIT.java
@@ -3,6 +3,8 @@ package tdt4140.gr1817.app.ui.feature.viewuser;
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.Provides;
+import com.google.inject.name.Named;
 import javafx.stage.Stage;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -13,10 +15,12 @@ import tdt4140.gr1817.app.ui.feature.util.NavigatorSpyModule;
 import tdt4140.gr1817.app.ui.javafx.JavaFxModule;
 import tdt4140.gr1817.app.ui.javafx.Navigator;
 import tdt4140.gr1817.app.ui.javafx.Page;
+import tdt4140.gr1817.ecosystem.persistence.Specification;
+import tdt4140.gr1817.ecosystem.persistence.data.ServiceProvider;
+import tdt4140.gr1817.ecosystem.persistence.data.ServiceProviderPermissions;
 import tdt4140.gr1817.ecosystem.persistence.data.User;
-import tdt4140.gr1817.ecosystem.persistence.repositories.RestingHeartRateRepository;
-import tdt4140.gr1817.ecosystem.persistence.repositories.UserRepository;
-import tdt4140.gr1817.ecosystem.persistence.repositories.WeightRepository;
+import tdt4140.gr1817.ecosystem.persistence.repositories.*;
+import tdt4140.gr1817.ecosystem.persistence.repositories.mysql.specification.GetServiceProviderByNameSpecification;
 
 import java.util.Calendar;
 import java.util.Collections;
@@ -40,14 +44,36 @@ public class ViewUserControllerIT extends ApplicationTest {
     private UserRepository userRepositoryMock;
     private WeightRepository weightRepositoryMock;
     private RestingHeartRateRepository restingHeartRateRepository;
+    private ServiceProviderRepository serviceProviderRepository;
+    private ServiceProviderPermissionsRepository permissionsRepository;
 
     @Override
     public void start(Stage stage) throws Exception {
         super.start(stage);
+
+        User user = new User(1, "", "", 185, new Date(), "", "", "");
+        ServiceProvider serviceProvider = new ServiceProvider(1, "improvify");
+        ServiceProviderPermissions providerPermissions = new ServiceProviderPermissions(user, serviceProvider,
+                true,
+                true,
+                true,
+                true,
+                true,
+                true,
+                true,
+                true
+        );
+
+
         MockedUserRepositoryModule mockedUserRepositoryModule = new MockedUserRepositoryModule();
         userRepositoryMock = mockedUserRepositoryModule.getUserRepositoryMock();
         weightRepositoryMock = Mockito.mock(WeightRepository.class);
         restingHeartRateRepository = Mockito.mock(RestingHeartRateRepository.class);
+        serviceProviderRepository = Mockito.mock(ServiceProviderRepository.class);
+        when(serviceProviderRepository.query(any(Specification.class)))
+                .thenReturn(Collections.singletonList(new ServiceProvider(1, "improvify")));
+        permissionsRepository = Mockito.mock(ServiceProviderPermissionsRepository.class);
+        when(permissionsRepository.query(any())).thenReturn(Collections.singletonList(providerPermissions));
 
         Injector injector = Guice.createInjector(
                 new JavaFxModule(stage),
@@ -58,6 +84,15 @@ public class ViewUserControllerIT extends ApplicationTest {
                     protected void configure() {
                         bind(WeightRepository.class).toInstance(weightRepositoryMock);
                         bind(RestingHeartRateRepository.class).toInstance(restingHeartRateRepository);
+                        bind(ServiceProviderRepository.class).toInstance(serviceProviderRepository);
+                        bind(ServiceProviderPermissionsRepository.class).toInstance(permissionsRepository);
+                    }
+
+                    @Provides
+                    @Named("improvify")
+                    public ServiceProvider providerImprovifyServiceProvider(ServiceProviderRepository repository) {
+                        return repository.query(new GetServiceProviderByNameSpecification("improvify")).get(0);
+
                     }
                 }
         );
