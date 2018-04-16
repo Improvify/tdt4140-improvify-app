de25bac9532dea777ff38c83827a9648b793f708
Henfau <henrik.fauskanger@gmail.com>
2018-04-16 19:38:00 +0200

    Add description and made for user to markdown format

--- tdt4140-gr1817/app.core/src/main/java/tdt4140/gr1817/app/core/feature/workoutplan/WorkoutplanToMarkdownConverter.java
+++ tdt4140-gr1817/app.core/src/main/java/tdt4140/gr1817/app/core/feature/workoutplan/WorkoutplanToMarkdownConverter.java
@@ -3,6 +3,7 @@ package tdt4140.gr1817.app.core.feature.workoutplan;
 
 import lombok.Data;
 import lombok.NonNull;
+import tdt4140.gr1817.ecosystem.persistence.data.User;
 import tdt4140.gr1817.ecosystem.persistence.data.improvify.WorkoutPlan;
 import tdt4140.gr1817.ecosystem.persistence.data.improvify.WorkoutPlanRow;
 import tdt4140.gr1817.ecosystem.persistence.repositories.improvify.WorkoutPlanRowRepository;
@@ -30,6 +31,11 @@ public class WorkoutplanToMarkdownConverter {
         List<WorkoutPlanRow> rows = workoutPlanRowRepository.query(specification);
 
         StringBuilder markdown = new StringBuilder();
+        User user = workoutplanToConvert.getCreatedForUser();
+
+        markdown.append("Created for " + user.getFirstName() + " " + user.getLastName() + "\n");
+        markdown.append(workoutplanToConvert.getDescription() + "\n");
+
         markdown.append("Description|Duration|Intensity|Comment\n");
         markdown.append("-----------|--------|---------|-------\n");
 
